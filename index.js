8486f3671f5fe627c15d8baef7449655305c5aba
unknown <lukastyon@gmail.com>
2024-03-26 18:50:57 -0500

    Add age verification with loop

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function verifyAdulthood(name, age) {
 console.log(verifyAdulthood(name, age));
 */
 
-const person = {
+/*const person = {
   name: "John",
   age: 18,
 };
@@ -24,3 +24,36 @@ function verifyAdulthood(personName, personAge) {
 const adultMessage = verifyAdulthood(person.name, person.age);
 
 console.log(adultMessage);
+*/
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
+
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
+
+  return `You are not old enough to enter, ${personName}.`;
+}
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(verifyAdulthood(person.name, person.age));
+}
